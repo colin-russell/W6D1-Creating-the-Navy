1107c71332d8b21fb43fb7536d3880de14ef008d
Colin Russell <colinrussell101@gmail.com>
2018-05-14 17:19:36 -0400

    added barebones of tables

--- navy_schema.sql
+++ navy_schema.sql
@@ -1,1 +1,19 @@
 SELECT 'hello, SQLite';
+
+CREATE TABLE fleet (
+  name TEXT
+);
+
+CREATE TABLE ships (
+  name TEXT,
+  date_built TEXT,
+  ship_id Integer
+);
+
+CREATE TABLE  sailor (
+
+);
+
+CREATE TABLE duties (
+
+);
